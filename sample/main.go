b8f7de10d3d68bb3459abb1597490b907d00054c
zubernetes <zubernetes@gmail.com>
2016-04-07 22:01:48 -0400

    Greatly simpled some of the List code in sample. Going to work tomorrow here.

--- sample/main.go
+++ sample/main.go
@@ -9,14 +9,13 @@ import (
 func main() {
     // Create a new list and put some numbers in it.
     l  := list.New()
-    e4 := l.PushBack(4)
-    e1 := l.PushFront(1)
-    
-    l.InsertBefore(3, e4)
-    l.InsertAfter(2, e1)
+    l.PushBack(1)
+    l.PushBack(2)
+    l.PushBack(3)
+    l.PushBack(4)
 
     // Iterate through list and print its contents.
-    for e := l.Front(); e != nil; e = e.Next() {
-        fmt.Println(e.Value)
+    for i := l.Front(); i != nil; i = i.Next() {
+        fmt.Println(i.Value)
     }
 }
