61cddc141d5e83fbf1852ab5c000f6599c8eead7
zubernetes <zubernetes@gmail.com>
2016-04-26 17:47:47 -0400

    Removing comments

--- sample/main.go
+++ sample/main.go
@@ -6,19 +6,11 @@ import (
 )
 
 func main() {
-    // Create a new list and put some numbers in it.
-    // Initialization.
-    l    := list.New()
-    list := list.New()
-
-    // Insertion.
-    l.PushBack(1)
-    l.PushBack(2)
-    l.PushBack(3)
-    l.PushBack(4)
-
-    // Iterate through list and print its contents.
-    for i := l.Front(); i != nil; i = i.Next() {
+    l := list.New()
+    for i := 0; i < 10; i++ {
+        l.PushBack(i)
+    }
+    for j := l.Front(); j != nil; j = j.Next() {
         fmt.Println(i.Value)
     }
 }
