f77567b20e0c5479b4680fb3d8fd3dff5e69080d
zubernetes <zubernetes@gmail.com>
2016-09-23 14:14:10 -0400

    Pushing up lint changes and updates after running vscode-go

--- sample/main.go
+++ sample/main.go
@@ -5,16 +5,16 @@
 package main
 
 import (
-    "container/list"
-    "fmt"
+	"container/list"
+	"fmt"
 )
 
 func main() {
-    l := list.New()
-    for i := 0; i < 10; i++ {
-        l.PushBack(i)
-    }
-    for j := l.Front(); j != nil; j = j.Next() {
-        fmt.Println(i.Value)
-    }
+	l := list.New()
+	for i := 0; i < 10; i++ {
+		l.PushBack(i)
+	}
+	for j := l.Front(); j != nil; j = j.Next() {
+		fmt.Println(j.Value)
+	}
 }
