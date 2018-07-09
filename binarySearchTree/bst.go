7c0af6ca22a79f2c7e5d942408f7baad5665cdbc
zubernetes <zubernetes@gmail.com>
2018-07-09 00:29:12 -0400

    args parsing needs exception handling

--- binarySearchTree/bst.go
+++ binarySearchTree/bst.go
@@ -8,18 +8,28 @@ import (
 )
 
 func printFile(path string, info os.FileInfo, err error) error {
+    // Prints FILE that is found in current directory
+    // and all sub-directories
     if err != nil {
         log.Print(err)
         return nil
     }
+
     fmt.Println(path)
+
     return nil
 }
 
 func main() {
     log.SetFlags(log.Lshortfile)
-    dir := os.Args[1]
+    dir := os.Args[1:]
+
+    // if dir == nil {
+    //    log.Fatal("Did not provide a directory path to explore");
+    // }
+
     err := filepath.Walk(dir, printFile)
+
     if err != nil {
         log.Fatal(err)
     }
