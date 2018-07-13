d140a65703723fb597e1cece1d8569709b0f40e7
zubernetes <zubernetes@gmail.com>
2018-07-12 22:35:24 -0400

    spinner animation

--- goroutines/buffered-channels.go
+++ goroutines/buffered-channels.go
@@ -1,11 +1,9 @@
 package main
 
-import "fmt"
-
-func main() {
-	ch := make(chan int, 2)
-	ch <- 1
-	ch <- 2
-	fmt.Println(<-ch)
-	fmt.Println(<-ch)
-}
+//func main() {
+//	ch := make(chan int, 2)
+//	ch <- 1
+//	ch <- 2
+//	fmt.Println(<-ch)
+//	fmt.Println(<-ch)
+//}
--- goroutines/channels.go
+++ goroutines/channels.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func sum(s []int, c chan int) {
 	sum := 0
 	for _, v := range s {
@@ -10,13 +8,13 @@ func sum(s []int, c chan int) {
 	c <- sum // send sum to c
 }
 
-func main() {
-	s := []int{7, 2, 8, -9, 4, 0}
+//func main() {
+//	s := []int{7, 2, 8, -9, 4, 0}
 
-	c := make(chan int)
-	go sum(s[:len(s)/2], c)
-	go sum(s[len(s)/2:], c)
-	x, y := <-c, <-c //receive from c
+//	c := make(chan int)
+//	go sum(s[:len(s)/2], c)
+//	go sum(s[len(s)/2:], c)
+//	x, y := <-c, <-c //receive from c
 
-	fmt.Println(x, y, x+y)
-}
+//	fmt.Println(x, y, x+y)
+//}
