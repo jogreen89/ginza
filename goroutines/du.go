4ea188f0c2a61c758fe4dee881cfc110df0dd0e6
zubernetes <zubernetes@gmail.com>
2018-07-13 22:42:49 -0400

    building disk usage program

---
+++ goroutines/du.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+func walkDir(dir string, fileSizes chan<- int64) {
+	for _, entry := range dirents(dir) {
+		if entry.IsDir() {
+			subdir := filepath.Join(dir, entry.Name)
+			walkDir(subdir, fileSizes)
+		} else {
+			fileSizes <- entry.Size()
+		}
+	}
+}
+
+// dirents returns the entries of directory dir
+func dirents(dir string) []os.FileInfo {
+	entries, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "du: %v\n", err)
+		return nil
+	}
+	return entries
+}
+
+func main() {
+	fmt.Println("Hello, World!")
+}
--- goroutines/spinner.go
+++ goroutines/spinner.go
@@ -5,12 +5,12 @@ import (
 	"time"
 )
 
-func main() {
-	go spinner(100 * time.Millisecond)
-	const n = 45
-	fibN := fib(n) // show
-	fmt.Printf("\rFibonacci(%d) = %d\n", n, fibN)
-}
+//func main() {
+//	go spinner(100 * time.Millisecond)
+//	const n = 45
+//	fibN := fib(n) // show
+//	fmt.Printf("\rFibonacci(%d) = %d\n", n, fibN)
+//}
 
 func spinner(delay time.Duration) {
 	for {
